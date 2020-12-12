83f7a8a26ad373a1c124836a39d183bb05d2fa26
NowshinPial <41447891+NowshinPial@users.noreply.github.com>
2020-12-12 17:55:51 -0500

    Update Operators.java

--- SeleniumClassFaruq/src/LabWork12_11/Operators.java
+++ SeleniumClassFaruq/src/LabWork12_11/Operators.java
@@ -3,8 +3,24 @@ package LabWork12_11;
 public class Operators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/*
+		 * Operators
+		 */
+
+		// Modulation, what is the modulation;
+		// arithmetic
+		// create a class for this argument
+
+		int n = 5;
+		int o = 2;
+		int p = 5 % 2;
+		System.out.println(p);
+
+		//increment
+				// create a class for this argument
+				for (int j = 10; j > 0; j--) {
+					System.out.println(j);
 	}
 
 }
