04e82cc0a026794440996ffef3ae01a02b67d5f7
NowshinPial <41447891+NowshinPial@users.noreply.github.com>
2020-12-12 17:57:45 -0500

    Update SuperClassSmallerClass.java

--- SeleniumClassFaruq/src/LabWork12_11/SuperClassSmallerClass.java
+++ SeleniumClassFaruq/src/LabWork12_11/SuperClassSmallerClass.java
@@ -3,8 +3,17 @@ package LabWork12_11;
 public class SuperClassSmallerClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// super class can handle smaller class but smaller cannot handle super
+				byte age=20;
+				int age1=20;
+				
+				int age3= age+age1;
+				
+				int a =30;
+				long b=30;
+				long c=a+b;
+				
 	}
 
 }
