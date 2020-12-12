49b01db7fe6e4de686d08f6d510ce26f420cdb55
NowshinPial <41447891+NowshinPial@users.noreply.github.com>
2020-12-12 17:57:11 -0500

    Update StringConcatenation.java

--- SeleniumClassFaruq/src/LabWork12_11/StringConcatenation.java
+++ SeleniumClassFaruq/src/LabWork12_11/StringConcatenation.java
@@ -3,8 +3,18 @@ package LabWork12_11;
 public class StringConcatenation {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// String only be concatenation
+				String Fname="Faruq";
+				String lname="Molla";
+				String fullname=Fname+lname;
+				System.out.println(fullname);
+				
+				String fullname1=Fname+"   "+lname;
+				System.out.println(fullname1);
+				
+				// + is overloaded operator.
+				
 	}
 
 }
