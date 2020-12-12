d94cc40f8fc9acb6df8fc6f0ee35fbc82aa4720d
NowshinPial <41447891+NowshinPial@users.noreply.github.com>
2020-12-12 17:56:48 -0500

    Update RelationalOperator.java

--- SeleniumClassFaruq/src/LabWork12_11/RelationalOperator.java
+++ SeleniumClassFaruq/src/LabWork12_11/RelationalOperator.java
@@ -3,7 +3,13 @@ package LabWork12_11;
 public class RelationalOperator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		// create a class for this argument
+		// equalsIgnoreCase for string data type.
+		String driver = "chrome";
+		if (driver.equalsIgnoreCase("chrome")) {
+			System.out.println("driver click");
+		}
 
 	}
 
